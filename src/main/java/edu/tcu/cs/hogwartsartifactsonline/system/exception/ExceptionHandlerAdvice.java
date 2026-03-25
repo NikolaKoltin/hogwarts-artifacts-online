4e3be93d9565b3600c8309b43bc28612d9c9a426
Nikola Koltin <nikoa.koltin@tcu.edu>
2026-03-25 01:22:58 -0500

    Wizard CRUD done! Close #2

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/system/exception/ExceptionHandlerAdvice.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/system/exception/ExceptionHandlerAdvice.java
@@ -1,6 +1,6 @@
 package edu.tcu.cs.hogwartsartifactsonline.system.exception;
 
-import edu.tcu.cs.hogwartsartifactsonline.artifact.ArtifactNotFoundException;
+import edu.tcu.cs.hogwartsartifactsonline.system.Result;
 import edu.tcu.cs.hogwartsartifactsonline.system.StatusCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
@@ -9,19 +9,17 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import edu.tcu.cs.hogwartsartifactsonline.system.Result;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
 
-    @ExceptionHandler(ArtifactNotFoundException.class)
+    @ExceptionHandler(ObjectNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    Result handleArtifactNotFoundException(ArtifactNotFoundException ex){
+    Result handleObjectNotFoundException(ObjectNotFoundException ex){
         return new Result(false, StatusCode.NOT_FOUND, ex.getMessage());
     }
 
